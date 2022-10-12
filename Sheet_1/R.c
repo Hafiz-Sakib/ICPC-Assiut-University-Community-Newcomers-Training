8d954217be788d3994558a05d16188c2aca13e9d
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-10-12 11:27:47 +0600

    R

--- Sheet_1/R.c
+++ Sheet_1/R.c
@@ -18,3 +18,48 @@ int main()
 
     return 0;
 }
+
+// My Dumb solution Which One was accepted by codeforces 🤦‍♂️🤦‍♂️🤦‍♂️🤦‍♂️🤦‍♂️🤦‍♂️🤦‍♂️ :
+
+/* #include <stdio.h>
+int main()
+{
+    int n, y, m, d, temp;
+    scanf("%d", &n);
+    if (n > 365)
+    {
+        y = n / 365;
+        n = n - (365 * y);
+        if ((n / 30 < 12))
+        {
+            m = n / 30;
+            n = n - (30 * m);
+            d = n;
+        }
+        else if ((n / 30 >= 12))
+        {
+            y++;
+            m = n - ((n / 30) * 30);
+            n = n - (30 * m);
+            d = n;
+        }
+    }
+    else if (n < 365)
+    {
+        y = 0;
+        m = n / 30;
+        n = n - (m * 30);
+        d = n;
+    }
+    else if (n == 365)
+    {
+        y = 1;
+        m = 0;
+        d = 0;
+    }
+    printf("%d years\n", y);
+    printf("%d months\n", m);
+    printf("%d days\n", d);
+
+    return 0;
+} */
--- Sheet_1/test.c
+++ Sheet_1/test.c
@@ -1,15 +1,42 @@
 #include <stdio.h>
 int main()
 {
-    int a, s, p, t, r;
-    scanf("%d", &a);
-    s = a / 365; /// 1
-    p = a % 365; /// 35
-    t = p / 30;
-    r = p % 30; /// 5
-    printf("%d years\n", s);
-    printf("%d months\n", t);
-    printf("%d days\n", r);
+    int n, y, m, d, temp;
+    scanf("%d", &n);
+    if (n > 365)
+    {
+        y = n / 365;
+        n = n - (365 * y);
+        if ((n / 30 < 12))
+        {
+            m = n / 30;
+            n = n - (30 * m);
+            d = n;
+        }
+        else if ((n / 30 >= 12))
+        {
+            y++;
+            m = n - ((n / 30) * 30);
+            n = n - (30 * m);
+            d = n;
+        }
+    }
+    else if (n < 365)
+    {
+        y = 0;
+        m = n / 30;
+        n = n - (m * 30);
+        d = n;
+    }
+    else if (n == 365)
+    {
+        y = 1;
+        m = 0;
+        d = 0;
+    }
+    printf("%d years\n", y);
+    printf("%d months\n", m);
+    printf("%d days\n", d);
 
     return 0;
 }
