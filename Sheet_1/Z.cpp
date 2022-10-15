c763a8d98fa2e18bd3f3c1acf1a597da8d8d886e
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-10-16 01:13:47 +0600

    Alhamdulillah

--- Sheet_1/Z.cpp
+++ Sheet_1/Z.cpp
@@ -7,10 +7,16 @@ int main()
 {
 
     ll res1, res2, a, b, c, d;
+
     cin >> a >> b >> c >> d;
+
     if ((b * log(a) < d * log(c)) || (b * log(a) == d * log(c)))
+    {
         cout << "NO";
+    }
     else
+    {
         cout << "YES";
+    }
     return 0;
 }
