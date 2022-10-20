a0e8e5d2a047b86d05f19d7c42730cf9aa2f1e89
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-10-20 21:05:00 +0600

    Alhamdulillah Contest 01 Completed

--- Contest 01/I.c
+++ Contest 01/I.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 int main()
 {
-    long long int a;
-    scanf("%lld", &a);
+    int a, r, t;
+    scanf("%d", &a);
+    r = a / 10;
+    t = a % 10;
+    /*  printf("%d\n", r);
+     printf("%d", t); */
+
+    if (t % r == 0 || r % t == 0)
+    {
+        printf("YES");
+    }
+    else
+    {
+        printf("NO");
+    }
     return 0;
 }
