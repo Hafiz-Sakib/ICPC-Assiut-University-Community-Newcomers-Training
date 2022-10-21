ee8184ab85500c254105fa02e455e853b16fd180
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-10-21 22:32:14 +0600

    I

--- Sheet_2(Loops)/I.c
+++ Sheet_2(Loops)/I.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 int main()
 {
-    int n, r, sum = 0;
+    int n, r, sum = 0, temp;
     scanf("%d", &n);
+    temp = n;
     while (n > 0)
     {
         r = n % 10;
         sum = sum * 10 + r;
         n = n / 10;
     }
-    if (r == n)
+
+    n = temp;
+
+    if (sum == n)
     {
-        printf("%d\nYES", r);
+        printf("%d\nYES", sum);
     }
 
     else
     {
-        printf("%d\nNO", r);
+        printf("%d\nNO", sum);
     }
     return 0;
 }
