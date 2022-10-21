644f89df1390c1ebb533220a7653b4a38b9c07a6
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-10-21 18:13:15 +0600

    Updates

--- Sheet_2(Loops)/G.c
+++ Sheet_2(Loops)/G.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 int main()
 {
+    long long int i, r = 1, t;
+    scanf("%lld", &t);
+    while (t--)
+    {
+        long long int n;
+        scanf("%lld", &n);
+        for (i = 1; i <= n; i++)
+        {
+            r = r * i;
+        }
+        printf("%lld\n", r);
+        r = 1;
+    }
 
     return 0;
 }
