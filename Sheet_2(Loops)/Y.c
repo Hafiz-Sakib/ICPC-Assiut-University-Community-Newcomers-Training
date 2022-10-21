77bb0efdbf5ac145fa72535f7bc88868bb5d11d9
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-10-21 14:42:33 +0600

    Y

---
+++ Sheet_2(Loops)/Y.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n, a = 0, b = 1, c = 0;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%d ", a);
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return 0;
+}
