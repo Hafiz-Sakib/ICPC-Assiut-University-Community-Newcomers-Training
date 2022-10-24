168e2d5817334c5f37ace2abdb09423a8281924a
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-10-24 12:40:31 +0600

    J

--- Sheet_2(Loops)/test.cpp
+++ Sheet_2(Loops)/test.cpp
@@ -1,7 +1,33 @@
+
+
 #include <iostream>
-#include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
+
+    int i, j, primeNum;
+    bool isPrim = true;
+
+    cin >> primeNum;
+
+    for (i = 2; i <= primeNum; i++)
+    {
+        for (j = 2; j < i; j++)
+        {
+            if (i % j == 0)
+            {
+                isPrim = false;
+            }
+        }
+
+        if (isPrim == true)
+        {
+            cout << i << " ";
+        }
+
+        isPrim = true;
+    }
+
     return 0;
 }
