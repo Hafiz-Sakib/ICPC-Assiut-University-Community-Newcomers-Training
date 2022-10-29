11483963ee86059fd3d31bb38886ce0247d742e6
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-10-29 10:16:26 +0600

    P

---
+++ Sheet_2(Loops)/P.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int a, i, j, b;
+    scanf("%d", &a);
+
+    for (i = a; i >= 1; i--)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
