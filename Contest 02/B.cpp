a9bcf727a42a361fc229c0b9f692549bab960284
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-02 10:36:10 +0600

    Contest 2 Problem A Solved

---
+++ Contest 02/B.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define cY cout << "YES\n"
+#define cN cout << "NO\n"
+#define cy cout << "Yes\n"
+#define cn cout << "No\n"
+#define space ' '
+#define newline "\n"
+int main()
+{
+    Boost;
+
+    return 0;
+}
