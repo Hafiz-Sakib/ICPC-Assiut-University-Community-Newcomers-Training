978cbb188e8218dc0159d1c579920fc4d9f3cf41
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-04 00:44:22 +0600

    Array A and B

---
+++ Sheet_3(Array)/B.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define cY cout << "YES\n"
+#define cN cout << "NO\n"
+#define cy cout << "Yes\n"
+#define cn cout << "No\n"
+#define space ' '
+#define newline "\n"
+#define read3         \
+    long int a, b, c; \
+    cin >> a >> b >> c;
+#define read2      \
+    long int a, b; \
+    cin >> a >> b;
+#define read1   \
+    long int a; \
+    cin >> a;
+#define write1 cout << a << newline;
+#define write2 cout << a << newline << b << newline;
+#define write3 cout << a << newline << b << newline << c << newline;
+#define fr(a, b) for (int i = a; i < b; i++)
+#define rep(i, a, b) for (int i = a; i < b; i++)
+int main()
+{
+    Boost;
+
+    long long int size, position = -1;
+    cin >> size;
+    long long int a[size];
+    for (int i = 0; i < size; i++)
+    {
+        cin >> a[i];
+    }
+    long long int num;
+    cin >> num;
+    for (int i = 0; i < size; i++)
+    {
+        if (a[i] == num)
+        {
+            position = i;
+            break;
+        }
+    }
+    cout << position;
+
+    return 0;
+}
