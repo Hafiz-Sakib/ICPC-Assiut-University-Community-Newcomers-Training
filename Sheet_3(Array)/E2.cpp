55860ca3b97186f9a29043d99fa78e8275028e43
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-04 10:20:20 +0600

    E2

--- Sheet_3(Array)/E2.cpp
+++ Sheet_3(Array)/E2.cpp
@@ -5,44 +5,32 @@ using namespace std;
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);                    \
     cout.tie(NULL)
-#define cY cout << "YES\n"
-#define cN cout << "NO\n"
-#define cy cout << "Yes\n"
-#define cn cout << "No\n"
 #define space ' '
-#define newline "\n"
-#define read3         \
-    long int a, b, c; \
-    cin >> a >> b >> c;
-#define read2      \
-    long int a, b; \
-    cin >> a >> b;
-#define read1   \
-    long int a; \
-    cin >> a;
-#define write1 cout << a << newline;
-#define write2 cout << a << newline << b << newline;
-#define write3 cout << a << newline << b << newline << c << newline;
-#define fr(a, b) for (int i = a; i < b; i++)
-#define rep(i, a, b) for (int i = a; i < b; i++)
+
 int main()
 {
     Boost;
 
-    long long int size, position;
+    int size, position = 1;
     cin >> size;
-    long long int a[size];
-    long long int mini = a[0];
+    int a[size];
+
     for (int i = 0; i < size; i++)
     {
         cin >> a[i];
+    }
+
+    int mini = a[0];
+
+    for (int i = 0; i < size; i++)
+    {
         if (a[i] < mini)
         {
             mini = a[i];
-            position = i;
+            position = i + 1;
         }
     }
-    cout << mini << space << position + 1;
+    cout << mini << space << position;
 
     return 0;
 }
