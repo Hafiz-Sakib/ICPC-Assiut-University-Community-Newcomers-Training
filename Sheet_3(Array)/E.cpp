675d2d506b44799d3edd1177753c338f3a79b535
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-04 09:55:40 +0600

    E

---
+++ Sheet_3(Array)/E.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+#define cY cout << "YES\n"
+#define cN cout << "NO\n"
+#define cy cout << "Yes\n"
+#define cn cout << "No\n"
+#define space ' '
+#define newline "\n"
+#define read3         \
+    long int a, b, c; \
+    cin >> a >> b >> c;
+#define read2      \
+    long int a, b; \
+    cin >> a >> b;
+#define read1   \
+    long int a; \
+    cin >> a;
+#define write1 cout << a << newline;
+#define write2 cout << a << newline << b << newline;
+#define write3 cout << a << newline << b << newline << c << newline;
+#define fr(a, b) for (int i = a; i < b; i++)
+#define rep(i, a, b) for (int i = a; i < b; i++)
+int main()
+{
+    Boost;
+
+    long long int size, maxi = LONG_LONG_MAX, mini, position;
+    cin >> size;
+    long long int a[size];
+    for (int i = 0; i < size; i++)
+    {
+        cin >> a[i];
+        if (a[i] < maxi)
+        {
+            mini = a[i];
+            maxi = mini;
+            position = i;
+        }
+    }
+    cout << mini << space << position + 1;
+
+    return 0;
+}
