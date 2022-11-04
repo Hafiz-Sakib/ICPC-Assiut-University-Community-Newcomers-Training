8eed3bb81fddec369605493101ab5b3a1ede5055
Hafiz-Sakib <hafizsakib5@gmail.com>
2022-11-04 20:42:30 +0600

    Z-Binary Search

--- Sheet_3(Array)/Binary__Search__STL.cpp
+++ Sheet_3(Array)/Binary__Search__STL.cpp
@@ -6,11 +6,6 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-#ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-#endif
-
     int n, k, i;
     cin >> n >> k;
     int arr[n];
@@ -22,8 +17,12 @@ int main()
         int x;
         cin >> x;
         if (binary_search(arr, arr + n, x))
+        {
             cout << "found" << '\n';
+        }
         else
+        {
             cout << "not found" << '\n';
+        }
     }
 }
