a6db0b77fc50945281be4a972a007055042212a7
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2022-12-13 23:45:57 +0600

    update

---
+++ Sheet_3(Array)/P-Minimize_Number.cpp
@@ -0,0 +1,61 @@
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+
+int main()
+{
+    Boost;
+    int n, cnt = 0, f = 0;
+    cin >> n;
+    vector<long long int> v(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
+    while (f == 0)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (v[i] % 2 != 0)
+            {
+                f = 1;
+                break;
+            }
+            else
+            {
+                v[i] = (v[i] / 2);
+                if (i == (n - 1))
+                {
+                    cnt++;
+                }
+            }
+        }
+    }
+
+    if (f)
+    {
+        cout << cnt << endl;
+    }
+    else
+    {
+
+        cout << 0 << endl;
+    }
+
+    return 0;
+}
