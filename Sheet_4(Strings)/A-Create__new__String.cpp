55318c52dd16fa4689cb75f16aaaf4ea35c11445
Mohammad Hafizur Rahman Sakib <96834409+Hafiz-Sakib@users.noreply.github.com>
2022-12-15 09:49:15 +0600

    update

---
+++ Sheet_4(Strings)/A-Create__new__String.cpp
@@ -0,0 +1,30 @@
+/*
+
+string author;
+author = Hafiz_Sakib;
+
+*/
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+#define endl "\n"
+#define ignore cin.ignore(numeric_limits<streamsize>::max(), '\n')
+#define Boost                         \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
+
+#define debug(x) cerr << x << endl;
+#define here fprintf(stderr, "====I am Here====\n");
+
+int main()
+{
+    Boost;
+
+    string s, p;
+    cin >> s >> p;
+    cout << s.size() << " " << p.size() << endl
+         << s << " " << p << endl;
+
+    return 0;
+}
